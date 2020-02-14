6424be61b310f7a09e81162b05bd2fdecb5346c4
Ashley Twomey <aprier091012@gmail.com>
2020-02-14 02:14:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,43 +7,38 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize (name, grade, id = nil)
-    @name = name
-    @grade = grade
-    @id = id
-  end
+  def initialize(id = nil, name, grade)
+@name = name
+@grade = grade
+@id = id
+end
 
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT,
-        grade INTEGER
-        )
-    SQL
-      DB[:conn].execute(sql)
-  end
+def self.create_table
+  sql = <<-SQL
+  create table students(id integer primary key, name text, grade text);
+  SQL
+  DB[:conn].execute(sql)
 
-  def self.drop_table
-    sql = <<-SQL
-      DROP TABLE students
-    SQL
+end
 
-    DB[:conn].execute(sql)  
-  end
+def self.drop_table
+sql = <<-SQL
+drop table students;
+SQL
+DB[:conn].execute(sql)
+end
 
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-        VALUES (?, ?)
-    SQL
+def save
+  sql = <<-SQL
+  insert into students (name,grade) values(?,?);
+  SQL
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
+end
 
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
-
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
-  end
+def self.create(name:, grade:)
+student = Student.new(name, grade)
+student.save
+student
+end
 end
